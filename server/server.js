983c85a0a2673157b9835379d98ad1c8498de571
Petros Mitakos <petros.mitakos@e-food.gr>
2018-12-05 00:56:18 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -28,6 +29,26 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send({errorMessage: 'Todo not found'})
+  }
+  else {
+      Todo.findById(id).then((todo) => {
+        if (todo) {
+          res.send({todo});
+        }
+        else {
+          res.status(404).send({errorMessage: 'Could not load todo.'})
+        }
+      }).catch((e) => {
+      res.status(400).send();
+    })
+  }
+})
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
